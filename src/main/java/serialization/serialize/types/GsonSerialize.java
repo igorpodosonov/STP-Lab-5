196099fdfa9772e901b60c35130f03de4711e50c
igorpodosonov <podosonov.work@gmail.com>
2017-12-07 19:14:42 +0200

    Tests was refactored

--- src/main/java/serialization/serialize/types/GsonSerialize.java
+++ src/main/java/serialization/serialize/types/GsonSerialize.java
@@ -11,15 +11,27 @@ public class GsonSerialize extends SuperSerialization {
     public void runSerialization(String filePath) throws IOException {
         measureStart();
 
-        String json = getTextFromFile(filePath);
+        ClassToSerialize serializedClass = serialize(filePath);
+        String deserialize = deserialize(serializedClass);
 
-        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();
-        ClassToSerialize serializedClass = gson.fromJson(json, ClassToSerialize.class);
-
-        String deserialize = gson.toJson(serializedClass);
         overwriteFile(filePath, deserialize);
 
         measuresEnd();
         displayStats("Gson Serialize/Deserialize");
+    }
+
+    public String deserialize(ClassToSerialize serializedClass){
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();
+        String deserialize = gson.toJson(serializedClass);
+
+        return deserialize;
+    }
+
+    public ClassToSerialize serialize(String filePath) {
+        String json = getTextFromFile(filePath);
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();
+        ClassToSerialize serializedClass = gson.fromJson(json, ClassToSerialize.class);
+
+        return serializedClass;
     }
 }
--- src/test/java/GsonTest.java
+++ src/test/java/GsonTest.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import serialization.*;
+import serialization.serialize.types.GsonSerialize;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -10,5 +11,45 @@ import java.io.IOException;
 import static org.junit.Assert.*;
 
 public class GsonTest {
+    private String filePath = "D://serialize-gson.txt";
+    private ClassToSerialize expectedClass;
+    private ClassToSerialize actual;
 
+    @Before
+    public void setUp(){
+        GsonSerialize gson = new GsonSerialize();
+        expectedClass = gson.serialize(filePath);
+    }
+
+    @Test
+    public void testSerialize() throws Exception {
+        GsonSerialize gson = new GsonSerialize();
+        actual = gson.serialize(filePath);
+
+        assertEquals(expectedClass.name, actual.name);
+        assertEquals(expectedClass.year, actual.year);
+
+        assertEquals(expectedClass.directorInfo.name, actual.directorInfo.name);
+        assertEquals(expectedClass.directorInfo.yearOfBirth, actual.directorInfo.yearOfBirth);
+        assertEquals(expectedClass.directorInfo.placeOfBirth, actual.directorInfo.placeOfBirth);
+
+        assertEquals(expectedClass.actorsRoles, actual.actorsRoles);
+    }
+
+    @Test
+    public void testDeserialize() throws Exception {
+        GsonSerialize gson = new GsonSerialize();
+        gson.deserialize(expectedClass);
+
+        actual = gson.serialize(filePath);
+
+        assertEquals(expectedClass.name, actual.name);
+        assertEquals(expectedClass.year, actual.year);
+
+        assertEquals(expectedClass.directorInfo.name, actual.directorInfo.name);
+        assertEquals(expectedClass.directorInfo.yearOfBirth, actual.directorInfo.yearOfBirth);
+        assertEquals(expectedClass.directorInfo.placeOfBirth, actual.directorInfo.placeOfBirth);
+
+        assertEquals(expectedClass.actorsRoles, actual.actorsRoles);
+    }
 }
